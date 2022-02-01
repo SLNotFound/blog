a914fb9b97376e3b22f77f9cc2095fcdc5c4e0e5
SunLightLei <1255193087@qq.com>
2022-02-01 10:05:28 +0800

    创建用户并保存到数据库

--- dao/mysql/user.go
+++ dao/mysql/user.go
@@ -1,9 +1,39 @@
 package mysql
 
-import "fmt"
+import (
+	"blog/models"
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+)
 
-// QueryUserByUsername 根据username查询用户是否存在
-func QueryUserByUsername(username string) {
-	sqlStr := fmt.Sprintf("select * from user where username=%s", username)
-	db.QueryRow(sqlStr)
+const secret = "test"
+
+// CheckUserExist 根据username查询用户是否存在
+func CheckUserExist(username string) (err error) {
+	sqlStr := `select count(user_id) from user where username = ?`
+	var count int
+	if err := db.Get(&count, sqlStr, username); err != nil {
+		return err
+	}
+	if count > 0 {
+		return errors.New("用户已存在")
+	}
+	return
+}
+
+func InsertUser(u *models.User) (err error) {
+	// 对密码进行加密
+	u.Password = encryptPassword(u.Password)
+	sqlStr := `insert into user(user_id, username, password) values (?, ?, ?)`
+	_, err = db.Exec(sqlStr, u.UserID, u.Username, u.Password)
+
+	return
+}
+
+// encryptPassword 加密密码
+func encryptPassword(oPassword string) string {
+	h := md5.New()
+	h.Write([]byte(secret))
+	return hex.EncodeToString(h.Sum([]byte(oPassword)))
 }
--- logic/user.go
+++ logic/user.go
@@ -6,11 +6,20 @@ import (
 	"blog/pkg/snowflake"
 )
 
-func SignUp(p *models.SignUpParam) {
+func SignUp(p *models.SignUpParam) (err error) {
 	// 判断用户是否存在，查询语句
-	mysql.QueryUserByUsername("")
+	if err := mysql.CheckUserExist(p.Username); err != nil {
+		return err
+	}
 
 	// 生成UID
-	snowflake.GenID()
+	userID := snowflake.GenID()
 
+	user := &models.User{
+		UserID:   userID,
+		Username: p.Username,
+		Password: p.Password,
+	}
+
+	return mysql.InsertUser(user)
 }
